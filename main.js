39ec035bf086455ef3d3c8a46b70d18dfe4c47ba
arifeensifat <arifeen.sifat@gmail.com>
2023-07-24 12:47:26 +0600

    Webside fully done

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 
-
+//  about me sction start 
 let tablinks = document.getElementsByClassName("tab-links");
 let tabcontents = document.getElementsByClassName("tab-contents");
 
@@ -13,3 +13,17 @@ function opentab(tabname){
        event.currentTarget.classList.add("active-link");
        document.getElementById(tabname).classList.add("active-tab")
 }
+
+// about me section end 
+
+// sidemenu section start 
+ 
+let sidemenu =document.getElementById("sidemenu");
+
+function openmenu (){
+     sidemenu.style.right ="0";
+}
+function closemenu (){
+     sidemenu.style.right ="-200px";
+}
+// sidemenu section end
